d8ce2b52c2bddd9a7edaf179c96c3ab17d8cf660
giayu23 <99064656+giayu23@users.noreply.github.com>
2022-07-02 22:14:44 -0700

    Initial commit

---
+++ springboot-backend/src/main/java/io/project/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package io.project.springboot.repository;
+
+import io.project.springboot.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    // all crud database methods
+}
+
+
